bd6ee5322a1f6f425bf01be8b4f2adc5e6a53244
Marco <makroxyz@gmail.com>
2014-12-16 09:58:59 +0100

    Update OnScreenKeyboard.php

--- OnScreenKeyboard.php
+++ OnScreenKeyboard.php
@@ -2,7 +2,41 @@
 
 namespace makroxyz\onscrkbd;
 
+use yii\helpers\Html;
+
 class OnScreenKeyboard extends \yii\base\Widget
 {
-	
+    public $firstInputSelector = ':input';
+    
+	public function init()
+    {
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        } else {
+            $this->id = $this->options['id'];
+        }
+        
+        echo Html::beginTag('div', $this->options);
+        $this->registerScript();
+    }
+    
+    public function run()
+    {
+        echo Html::endTag('div');
+    }
+    
+    protected function registerScript()
+    {
+        $view = $this->getView();
+        OnScreenKeyboardAsset::register($view);
+        
+        $view->registerJs("
+        $(function () {
+            jsKeyboard.init('{$this->id}');
+            //first input focus
+            var \$firstInput = $('{$this->firstInputSelector}').first().focus();
+            jsKeyboard.currentElement = \$firstInput;
+            jsKeyboard.currentElementCursorPosition = 0;
+        });");
+    }
 }
